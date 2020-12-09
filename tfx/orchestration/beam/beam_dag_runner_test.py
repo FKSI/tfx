5e2d4c5a909200b6cc466edcce9e031328aacb61
tfx-team <tensorflow-extended-nonhuman@googlegroups.com>
2020-12-08 20:05:44 -0800

    Add fields to the pipeline spec and DB schema to support sub DAG feature.
    PiperOrigin-RevId: 346465478

--- tfx/orchestration/beam/beam_dag_runner_test.py
+++ tfx/orchestration/beam/beam_dag_runner_test.py
@@ -24,17 +24,12 @@ from tfx.orchestration.portable import test_utils
 from tfx.proto.orchestration import executable_spec_pb2
 from tfx.proto.orchestration import local_deployment_config_pb2
 from tfx.proto.orchestration import pipeline_pb2
-from tfx.proto.orchestration import platform_config_pb2
 
 from google.protobuf import message
 from google.protobuf import text_format
 
 _PythonClassExecutableSpec = executable_spec_pb2.PythonClassExecutableSpec
-_ContainerExecutableSpec = executable_spec_pb2.ContainerExecutableSpec
-_DockerPlatformConfig = platform_config_pb2.DockerPlatformConfig
-
-_LOCAL_DEPLOYMENT_CONFIG = text_format.Parse(
-    """
+_LOCAL_DEPLOYMENT_CONFIG = text_format.Parse("""
     executor_specs {
       key: "my_example_gen"
       value {
@@ -54,8 +49,8 @@ _LOCAL_DEPLOYMENT_CONFIG = text_format.Parse(
     executor_specs {
       key: "my_trainer"
       value {
-        container_executable_spec {
-          image: "path/to/docker/image"
+        python_class_executable_spec {
+          class_path: "tfx.components.trainer_executor"
         }
       }
     }
@@ -70,18 +65,9 @@ _LOCAL_DEPLOYMENT_CONFIG = text_format.Parse(
     metadata_connection_config {
       fake_database {}
     }
-    node_level_platform_configs {
-      key: "my_trainer"
-      value {
-        docker_platform_config {
-          docker_server_url: "docker/server/url"
-        }
-      }
-    }
 """, local_deployment_config_pb2.LocalDeploymentConfig())
 
-_INTERMEDIATE_DEPLOYMENT_CONFIG = text_format.Parse(
-    """
+_INTERMEDIATE_DEPLOYMENT_CONFIG = text_format.Parse("""
     executor_specs {
       key: "my_example_gen"
       value {
@@ -101,8 +87,8 @@ _INTERMEDIATE_DEPLOYMENT_CONFIG = text_format.Parse(
     executor_specs {
       key: "my_trainer"
       value {
-        [type.googleapis.com/tfx.orchestration.executable_spec.ContainerExecutableSpec] {
-          image: "path/to/docker/image"
+        [type.googleapis.com/tfx.orchestration.executable_spec.PythonClassExecutableSpec] {
+          class_path: "tfx.components.trainer_executor"
         }
       }
     }
@@ -119,20 +105,11 @@ _INTERMEDIATE_DEPLOYMENT_CONFIG = text_format.Parse(
         fake_database {}
       }
     }
-    node_level_platform_configs {
-      key: "my_trainer"
-      value {
-        [type.googleapis.com/tfx.orchestration.platform_config.DockerPlatformConfig] {
-          docker_server_url: "docker/server/url"
-        }
-      }
-    }
 """, pipeline_pb2.IntermediateDeploymentConfig())
 
 _executed_components = []
 _component_executors = {}
 _component_drivers = {}
-_component_platform_configs = {}
 _conponent_to_pipeline_run = {}
 
 
@@ -153,8 +130,6 @@ class _FakeComponentAsDoFn(beam_dag_runner.PipelineNodeAsDoFn):
                      deployment_config)
     _component_executors[self._node_id] = executor_spec
     _component_drivers[self._node_id] = custom_driver_spec
-    _component_platform_configs[self._node_id] = self._extract_platform_config(
-        self._deployment_config, self._node_id)
     pipeline_run = None
     for context in pipeline_node.contexts.contexts:
       if context.type.name == constants.PIPELINE_RUN_ID_PARAMETER_NAME:
@@ -178,7 +153,6 @@ class BeamDagRunnerTest(test_utils.TfxTest):
     _executed_components.clear()
     _component_executors.clear()
     _component_drivers.clear()
-    _component_platform_configs.clear()
     _conponent_to_pipeline_run.clear()
 
   @mock.patch.multiple(
@@ -186,7 +160,7 @@ class BeamDagRunnerTest(test_utils.TfxTest):
       _PIPELINE_NODE_DO_FN_CLS=_FakeComponentAsDoFn,
   )
   def testRunWithLocalDeploymentConfig(self):
-    self._pipeline.deployment_config.Pack(_LOCAL_DEPLOYMENT_CONFIG)
+    self._pipeline.deployment_config.Pack(_INTERMEDIATE_DEPLOYMENT_CONFIG)
     beam_dag_runner.BeamDagRunner().run(self._pipeline)
     self.assertEqual(
         _component_executors, {
@@ -199,10 +173,10 @@ class BeamDagRunnerTest(test_utils.TfxTest):
                     'class_path: "tfx.components.transform_executor"',
                     _PythonClassExecutableSpec()),
             'my_trainer':
-                text_format.Parse('image: "path/to/docker/image"',
-                                  _ContainerExecutableSpec()),
-            'my_importer':
-                None,
+                text_format.Parse(
+                    'class_path: "tfx.components.trainer_executor"',
+                    _PythonClassExecutableSpec()),
+            'my_importer': None,
         })
     self.assertEqual(
         _component_drivers, {
@@ -210,24 +184,9 @@ class BeamDagRunnerTest(test_utils.TfxTest):
                 text_format.Parse(
                     'class_path: "tfx.components.example_gen_driver"',
                     _PythonClassExecutableSpec()),
-            'my_transform':
-                None,
-            'my_trainer':
-                None,
-            'my_importer':
-                None,
-        })
-    self.assertEqual(
-        _component_platform_configs, {
-            'my_example_gen':
-                None,
-            'my_transform':
-                None,
-            'my_trainer':
-                text_format.Parse('docker_server_url: "docker/server/url"',
-                                  _DockerPlatformConfig()),
-            'my_importer':
-                None,
+            'my_transform': None,
+            'my_trainer': None,
+            'my_importer': None,
         })
     # 'my_importer' has no upstream and can be executed in any order.
     self.assertIn('my_importer', _executed_components)
@@ -242,7 +201,7 @@ class BeamDagRunnerTest(test_utils.TfxTest):
       _PIPELINE_NODE_DO_FN_CLS=_FakeComponentAsDoFn,
   )
   def testRunWithIntermediateDeploymentConfig(self):
-    self._pipeline.deployment_config.Pack(_INTERMEDIATE_DEPLOYMENT_CONFIG)
+    self._pipeline.deployment_config.Pack(_LOCAL_DEPLOYMENT_CONFIG)
     beam_dag_runner.BeamDagRunner().run(self._pipeline)
     self.assertEqual(
         _component_executors, {
@@ -255,10 +214,10 @@ class BeamDagRunnerTest(test_utils.TfxTest):
                     'class_path: "tfx.components.transform_executor"',
                     _PythonClassExecutableSpec()),
             'my_trainer':
-                text_format.Parse('image: "path/to/docker/image"',
-                                  _ContainerExecutableSpec()),
-            'my_importer':
-                None,
+                text_format.Parse(
+                    'class_path: "tfx.components.trainer_executor"',
+                    _PythonClassExecutableSpec()),
+            'my_importer': None,
         })
     self.assertEqual(
         _component_drivers, {
@@ -266,24 +225,9 @@ class BeamDagRunnerTest(test_utils.TfxTest):
                 text_format.Parse(
                     'class_path: "tfx.components.example_gen_driver"',
                     _PythonClassExecutableSpec()),
-            'my_transform':
-                None,
-            'my_trainer':
-                None,
-            'my_importer':
-                None,
-        })
-    self.assertEqual(
-        _component_platform_configs, {
-            'my_example_gen':
-                None,
-            'my_transform':
-                None,
-            'my_trainer':
-                text_format.Parse('docker_server_url: "docker/server/url"',
-                                  _DockerPlatformConfig()),
-            'my_importer':
-                None,
+            'my_transform': None,
+            'my_trainer': None,
+            'my_importer': None,
         })
     # 'my_importer' has no upstream and can be executed in any order.
     self.assertIn('my_importer', _executed_components)
@@ -292,7 +236,6 @@ class BeamDagRunnerTest(test_utils.TfxTest):
                      ['my_example_gen', 'my_transform', 'my_trainer'])
     # Verifies that every component gets a not-None pipeline_run.
     self.assertTrue(all(_conponent_to_pipeline_run.values()))
-
 
 if __name__ == '__main__':
   tf.test.main()
